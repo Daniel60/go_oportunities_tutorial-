3ca4da561e8cff31fa1cd7ec4cbe87d8ca32d106
Daniel60 <39488923+Daniel60@users.noreply.github.com>
2024-06-27 17:29:34 -0300

    feat: criado logica list openings

--- handler/ListOpening.go
+++ handler/ListOpening.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Daniel60/go_oportunities_tutorial/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpening(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "List Opening",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
 }
