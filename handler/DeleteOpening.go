249460d33fe3da674de70f8a61f8584193255139
Daniel60 <39488923+Daniel60@users.noreply.github.com>
2024-06-27 17:28:15 -0300

    feat: criado logica delete opening

--- handler/DeleteOpening.go
+++ handler/DeleteOpening.go
@@ -1,13 +1,28 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Daniel60/go_oportunities_tutorial/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteOpenings(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Delete Opening",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+	// Find Opening
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+	//Delete Opening
+	if err := db.Delete(&opening).Error; err != nil {
+		return
+	}
+	sendSuccess(ctx, "delete-opening", opening)
 }
